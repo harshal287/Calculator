cd4c6bdf47f900e9e443eda37084141c9fe1b257
harshal agrawal <harshal@harshals-macbook-pro.local>
2016-07-02 15:35:39 -0700

    TestClass file was added to the project

---
+++ Calculator/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  Calculator
+//
+//  Created by harshal agrawal on 7/2/16.
+//  Copyright © 2016 harshal. All rights reserved.
+//
+
+import Foundation
+
+class TestClass: NSObject {
+
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     var hasStoppedTyping = false
