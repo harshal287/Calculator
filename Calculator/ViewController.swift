5546a90af60ef58f80d157d5c5397c1ea7d1e2e3
harshal agrawal <harshal@harshals-macbook-pro.local>
2016-07-02 15:28:00 -0700

    Added Print to ViewDid load method

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -64,6 +64,7 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hi GIt")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
