b11544a303b2f820c906da483deea35b73054a37
harshal agrawal <harshal@harshals-MacBook-Pro.local>
2016-07-02 13:36:43 -0700

    Initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by harshal agrawal on 7/1/16.
+//  Copyright © 2016 harshal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var hasStoppedTyping = false
+    var firstDiplayNum = ""
+    var SecondDisplayNum = ""
+    var displayContainer = ""
+    @IBOutlet weak var Display: UILabel!
+    @IBAction func OperatorButton(sender: UIButton) {
+        
+        
+        
+        if firstDiplayNum == "" || firstDiplayNum == displayContainer {
+            firstDiplayNum = displayContainer
+            displayContainer = ""
+        } else {
+            SecondDisplayNum = displayContainer
+            displayContainer = ""
+        }
+            
+        if SecondDisplayNum != "" {
+            switch sender.currentTitle! {
+            case "×" :
+                displayContainer = "\(Double(firstDiplayNum)! * Double(SecondDisplayNum)!)"
+            case "÷" :
+                displayContainer = "\(Double(firstDiplayNum)! / Double(SecondDisplayNum)!)"
+            case "+" :
+                displayContainer = "\(Double(firstDiplayNum)! + Double(SecondDisplayNum)!)"
+            case "−" :
+                displayContainer = "\(Double(firstDiplayNum)! - Double(SecondDisplayNum)!)"
+            default : break
+            }
+            
+            Display.text = displayContainer
+            firstDiplayNum = displayContainer
+            SecondDisplayNum = ""
+            hasStoppedTyping = true
+            
+        }
+        
+        
+        
+        
+        
+        
+    }
+    @IBAction func Numeric(sender: UIButton) {
+        if hasStoppedTyping {
+            displayContainer = ""
+        }
+        displayContainer = displayContainer + sender.currentTitle!
+       Display.text = displayContainer
+        hasStoppedTyping = false
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
